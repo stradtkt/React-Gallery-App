890703c9f4722b969a4d78a77257841aae2f482d
Kevin <stradtkt22@gmail.com>
2019-02-17 20:44:12 -0500

    Added NotFound

---
+++ src/components/NotFound.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NotFound = () => {
+    const iconStyles = {
+        fontSize: '165px',
+        color: 'red',
+        margin: '0 auto'
+    }
+    const divStyles = {
+        margin: '0 auto'
+    }
+    return (
+        <div className="not-found-page" style={divStyles}>
+            <i className="material-icons" style={iconStyles}>report </i>
+            <p>I am sorry that page is unavailable please try again.</p>
+        </div>
+    )
+};
+
+export default NotFound;
