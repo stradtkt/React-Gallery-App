12f63fa18cefbf090b0650d7b07de279a228a863
Kevin <stradtkt22@gmail.com>
2019-02-17 20:49:57 -0500

    Added loading

---
+++ src/components/Loading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+const Loading = ({ type, color }) => (
+    <ReactLoading type={this.bubbles} color={this.blue} height="700" width="400"/>
+);
+
+export default Loading;
